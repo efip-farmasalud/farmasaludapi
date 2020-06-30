140af4040ad9daf38afeb260b7e9fbcf410b821e
Matias Gonzalo calvo <matiasgonzalocalvo@gmail.com>
2020-06-30 00:46:05 -0300

    update openid realm

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ flask_app.config.update({
     'OIDC_ID_TOKEN_COOKIE_SECURE': False,
     'OIDC_REQUIRE_VERIFIED_EMAIL': False,
     'OIDC_USER_INFO_ENABLED': True,
-    'OIDC_OPENID_REALM': 'flask-demo',
+    'OIDC_OPENID_REALM': 'farmasalud',
     'OIDC_SCOPES': ['openid', 'email', 'profile'],
     'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post'
 })
